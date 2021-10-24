6686e5b079f7053bc32e855aa31f377340b83b44
daria <61759834+jezowska@users.noreply.github.com>
2021-10-24 15:12:59 +0200

    added settings

---
+++ UP_Kamera/UP_Kamera/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace UP_Kamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WebCamBox = new System.Windows.Forms.PictureBox();
+            this.buttonRecord = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonSettings = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.WebCamBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WebCamBox
+            // 
+            this.WebCamBox.Location = new System.Drawing.Point(45, 12);
+            this.WebCamBox.Name = "WebCamBox";
+            this.WebCamBox.Size = new System.Drawing.Size(483, 302);
+            this.WebCamBox.TabIndex = 0;
+            this.WebCamBox.TabStop = false;
+            this.WebCamBox.Click += new System.EventHandler(this.WebCamBox_Click);
+            // 
+            // buttonRecord
+            // 
+            this.buttonRecord.Location = new System.Drawing.Point(453, 342);
+            this.buttonRecord.Name = "buttonRecord";
+            this.buttonRecord.Size = new System.Drawing.Size(75, 35);
+            this.buttonRecord.TabIndex = 5;
+            this.buttonRecord.Text = "Record";
+            this.buttonRecord.UseVisualStyleBackColor = true;
+            this.buttonRecord.Click += new System.EventHandler(this.buttonRecord_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(45, 342);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 35);
+            this.buttonStart.TabIndex = 6;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(182, 342);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 35);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonSettings
+            // 
+            this.buttonSettings.Location = new System.Drawing.Point(316, 342);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(75, 35);
+            this.buttonSettings.TabIndex = 8;
+            this.buttonSettings.Text = "Settings";
+            this.buttonSettings.UseVisualStyleBackColor = true;
+            this.buttonSettings.Click += new System.EventHandler(this.buttonSettings_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(453, 383);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 35);
+            this.buttonStop.TabIndex = 9;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 450);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonSettings);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonRecord);
+            this.Controls.Add(this.WebCamBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.WebCamBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox WebCamBox;
+        private System.Windows.Forms.Button buttonRecord;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonSettings;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ UP_Kamera/UP_Kamera/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UP_Kamera
+{
+    public partial class Form1 : Form
+    {
+        WebCam webCam = new WebCam();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void WebCamBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            webCam.Container = WebCamBox;
+            webCam.OpenConnection();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            webCam.SaveImage();
+        }
+
+        private void buttonSettings_Click(object sender, EventArgs e)
+        {
+            webCam.OpenSettings();
+        }
+
+        private void buttonRecord_Click(object sender, EventArgs e)
+        {
+            webCam.StartRecording();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            webCam.StopRecording();
+        }
+    }
+}
